2347a496530459b8732e798b81a4ea8d672a27dd
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-29 15:49:41 -0300

    03:04 - Diárias - Provider

---
+++ src/data/reduces/DiariasReducer.tsx
@@ -0,0 +1,63 @@
+import { useContext, useReducer } from "react";
+import { DiariaInterface } from "data/@types/DiariaInterface";
+import produce from "immer";
+import { UserContext } from "data/contexts/UserContext";
+import useApiHateoas from "data/hooks/useApi.hook";
+import { useEffect } from "@storybook/addons";
+export const initialState = {
+  diarias: [] as DiariaInterface[],
+  isFetching: true,
+};
+
+type InitialStateType = typeof initialState;
+
+type DiariaAction = "SET_DIARIA" | "SET_FETCHING";
+
+export type DiariaActionType = {
+  type: DiariaAction;
+  payload?: unknown;
+};
+
+const reducer = (
+  state: InitialStateType,
+  action: DiariaActionType
+): InitialStateType => {
+  const nextState = produce(state, (draftState) => {
+    switch (action.type) {
+      case "SET_DIARIA":
+        draftState.diarias = action.payload as DiariaInterface[];
+        draftState.isFetching = false;
+        break;
+      case "SET_FETCHING":
+        draftState.isFetching = action.payload as boolean;
+        break;
+    }
+  });
+
+  return nextState;
+};
+
+export interface DiariaReducerInterface {
+  diariaState: InitialStateType;
+  diariaDispatch: React.Dispatch<DiariaActionType>;
+}
+
+export function useDiariaReducer(): DiariaReducerInterface {
+  const [state, dispatch] = useReducer(reducer, initialState),
+    { userState } = useContext(UserContext),
+    diarias = useApiHateoas<DiariaInterface[]>(
+      userState.user.links,
+      "lista_diarias"
+    );
+
+  useEffect(() => {
+    if (diarias) {
+      dispatch({ type: "SET_DIARIA", payload: diarias });
+    }
+  }, [diarias]);
+
+  return {
+    diariaState: state,
+    diariaDispatch: dispatch,
+  };
+}
