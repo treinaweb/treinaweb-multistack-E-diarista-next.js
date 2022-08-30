bae4655deffabb73ee45d09e690258350c3fcb27
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-30 14:51:19 -0300

    03:11 - Inicio da página de detalhes da diária

--- src/pages/diarias.tsx
+++ src/pages/diarias.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { GetStaticProps } from "next";
 import MinhasDiarias from "@patials/diarias/_minhas-diarias";
 import { DiariaProvider } from "data/contexts/DiariaContext";
+import { useRouter } from "next/router";
+import DetalheDiaria from "@patials/diarias/_detalhe-diaria";
 
 // import { Component } from '@styles/pages/diarias.styled';
 
@@ -14,6 +16,16 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Diarias: React.FC = () => {
+  const router = useRouter();
+
+  if (router.query.id) {
+    return (
+      <DiariaProvider>
+        <DetalheDiaria id={router.query.id as string} />
+      </DiariaProvider>
+    );
+  }
+
   return (
     <DiariaProvider>
       <MinhasDiarias />
