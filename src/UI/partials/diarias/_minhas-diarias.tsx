711f9b81e1aa3a8306ce9c3a4f09f6a2f907585b
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-30 14:00:11 -0300

    03:10 - Exibindo diárias no data list

--- src/UI/partials/diarias/_minhas-diarias.tsx
+++ src/UI/partials/diarias/_minhas-diarias.tsx
@@ -3,6 +3,7 @@ import useMinhasDiarias from "data/hooks/pages/diarias/useMinhasDiarias.page";
 import { DiariaService } from "data/services/DiariaService";
 import { TextFormatService } from "data/services/TextFormatService";
 import React, { PropsWithChildren } from "react";
+import DataList from "UI/components/data-display/DataList/DataList";
 import PageTitle from "UI/components/data-display/PageTitle/PageTitle";
 import Status from "UI/components/data-display/Status/Status";
 import Table, {
@@ -28,7 +29,47 @@ const MinhasDiarias: React.FC<PropsWithChildren> = () => {
       <PageTitle title="Minhas diárias" />
       {filteredData.length > 0 ? (
         isMobile ? (
-          ""
+          <>
+            {filteredData.map((item) => {
+              return (
+                <DataList
+                  key={item.id}
+                  header={
+                    <>
+                      Data:
+                      {TextFormatService.reverseDate(
+                        item.data_atendimento as string
+                      )}
+                      <br />
+                      {item.nome_servico}
+                    </>
+                  }
+                  body={
+                    <>
+                      Status: {DiariaService.getStatus(item.status!).label}
+                      <br />
+                      Valor: {TextFormatService.currency(item.preco)}
+                    </>
+                  }
+                  actions={
+                    <>
+                      <Button
+                        component={Link}
+                        href=""
+                        color={"inherit"}
+                        variant={"outlined"}
+                      >
+                        Detalhes
+                      </Button>
+                      <Button color={"error"} variant={"contained"}>
+                        Cancelado
+                      </Button>
+                    </>
+                  }
+                />
+              );
+            })}
+          </>
         ) : (
           <>
             <Table
