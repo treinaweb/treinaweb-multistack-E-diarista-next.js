a741fc35419110c01db3f594582f8e5bf420970c
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-31 15:47:59 -0300

    03:07 - Montagem do formulário reutilizável para seleção de cidades

--- src/UI/components/inputs/UserForm/forms/CitiesForm.tsx
+++ src/UI/components/inputs/UserForm/forms/CitiesForm.tsx
@@ -8,5 +8,50 @@ import ChipField from "UI/components/data-display/ChipField/ChipField";
 export const CitiesForm: React.FC<{ estado: string }> = ({ estado }) => {
   const { options, handleNewCity, citiesList, citiesName, handleDelete } =
     useCitiesForm(estado);
-  return <CitiesSelecion></CitiesSelecion>;
+  return (
+    <CitiesSelecion>
+      <Autocomplete
+        value={{ cidade: "" } as CidadeInterface}
+        onChange={(_event, newValue) => {
+          newValue && handleNewCity(newValue.cidade);
+        }}
+        disablePortal
+        options={options}
+        getOptionLabel={(option) => option.cidade}
+        loading={citiesList.length === 0}
+        loadingText={"Carregando cidades..."}
+        style={{ gridArea: "busca-cidade" }}
+        noOptionsText={"Nenhuma cidade con esse nome"}
+        renderInput={({ InputProps, ...params }) => {
+          return (
+            <TextField
+              label={"Busque pelo nome da cidade"}
+              InputProps={{
+                ...InputProps,
+                endAdornment: (
+                  <>
+                    {citiesList.length ? (
+                      <i className="twf-search" />
+                    ) : (
+                      <CircularProgress />
+                    )}
+                    {InputProps.endAdornment}
+                  </>
+                ),
+              }}
+              required={false}
+              {...params}
+            />
+          );
+        }}
+      />
+
+      <Typography>Cidades selecionadas</Typography>
+      <ChipField
+        itemsList={citiesName}
+        onDelete={handleDelete}
+        emptyMessage={"Nenhuma cidade selecionada ainda"}
+      />
+    </CitiesSelecion>
+  );
 };
