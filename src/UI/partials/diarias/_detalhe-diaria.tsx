a742d6a5e3f3f1769c75d88360f02cc3c3deb0c6
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-31 09:27:00 -0300

    03:15 - Exibinção dos dados da diária

--- src/UI/partials/diarias/_detalhe-diaria.tsx
+++ src/UI/partials/diarias/_detalhe-diaria.tsx
@@ -1,9 +1,14 @@
-import { CircularProgress, Container } from "@mui/material";
+import { Box, CircularProgress, Container, Typography } from "@mui/material";
 import useDetalhesDiaria from "data/hooks/pages/diarias/useDetalhesDiaria.page";
+import { DateService } from "data/services/DateService";
+import { DiariaService } from "data/services/DiariaService";
+import { TextFormatService } from "data/services/TextFormatService";
 import React, { PropsWithChildren } from "react";
 import PageTitle from "UI/components/data-display/PageTitle/PageTitle";
+import Status from "UI/components/data-display/Status/Status";
+import UserInformation from "UI/components/data-display/UserInformation/UserInformation";
 
-// import { Component } from './_detalhe-diaria.styled';
+import { CardsContainer, JobDetails, JobTitle } from "./_detalhe-diaria.styled";
 
 const DetalheDiaria: React.FC<PropsWithChildren<{ id: string }>> = ({ id }) => {
   const { cliente, diaria, diarista } = useDetalhesDiaria(id);
@@ -19,6 +24,30 @@ const DetalheDiaria: React.FC<PropsWithChildren<{ id: string }>> = ({ id }) => {
   return (
     <Container>
       <PageTitle title={`Detalhes da diária: #${id}`} />
+      <CardsContainer>
+        <JobDetails>
+          <JobTitle>Detalhes da diária</JobTitle>
+          <Box sx={{ mb: 2 }}>
+            Status:
+            <Status colors={DiariaService.getStatus(diaria.status!).color}>
+              {DiariaService.getStatus(diaria.status!).label}
+            </Status>
+          </Box>
+          <div>
+            Data:{" "}
+            <strong>
+              {TextFormatService.reverseDate(diaria.data_atendimento as string)}
+            </strong>
+            <br />
+            Horário:{" "}
+            <strong>
+              {DateService.getTimeFromDate(diaria.data_atendimento as string)}
+            </strong>
+            <br />
+            Endereço: <strong>{TextFormatService.getAddress(diaria)}</strong>
+          </div>
+        </JobDetails>
+      </CardsContainer>
     </Container>
   );
 };
