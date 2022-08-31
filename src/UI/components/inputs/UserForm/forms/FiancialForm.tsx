c28c058a09f6b90531cee98c31db69eee0f7ba8a
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-08-31 11:51:44 -0300

    03:03 - Formulário reutilizável de modos de pagamento

---
+++ src/UI/components/inputs/UserForm/forms/FiancialForm.tsx
@@ -0,0 +1,17 @@
+import { FormValues } from "data/@types/forms/FormValue";
+import { useFormContext } from "react-hook-form";
+import TextField from "UI/components/inputs/TextField/TextField";
+import { FinancialData } from "../UserForm.styled";
+
+export const FinalcialForm = () => {
+  const { register } = useFormContext<FormValues>();
+  return (
+    <FinancialData>
+      <TextField
+        label={"Chave Pix"}
+        defaultValue={""}
+        {...register("usuario.chave_pix", { minLength: 5 })}
+      />
+    </FinancialData>
+  );
+};
