df4243864c07b11ebc5bfad0e57d69a8b80d86f6
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-09-14 11:31:24 -0300

    03:02 - Ajustes nos formulários: inicialização de valores

--- src/UI/components/inputs/UserForm/forms/FiancialForm.tsx
+++ src/UI/components/inputs/UserForm/forms/FiancialForm.tsx
@@ -1,15 +1,20 @@
 import { FormValues } from "data/@types/forms/FormValue";
+import { UserContext } from "data/contexts/UserContext";
+import { useContext } from "react";
 import { useFormContext } from "react-hook-form";
 import TextField from "UI/components/inputs/TextField/TextField";
 import { FinancialData } from "../UserForm.styled";
 
 const FinalcialForm = () => {
-  const { register } = useFormContext<FormValues>();
+  const { register } = useFormContext<FormValues>(),
+    {
+      userState: { user },
+    } = useContext(UserContext);
   return (
     <FinancialData>
       <TextField
         label={"Chave Pix"}
-        defaultValue={""}
+        defaultValue={user.chave_pix}
         {...register("usuario.chave_pix", { minLength: 5 })}
       />
     </FinancialData>
